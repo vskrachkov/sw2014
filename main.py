883b647027eb6a4e349fb8aed5ff71fd24f98203
Vyacheslav Krachkov <vskrachkov@gmail.com>
2018-01-27 11:42:20 +0200

    create loop for folders listing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from utils import start_sw, open_doc
 
-app = start_sw(sw_version='2014', visible=1)
-doc = open_doc(app, 'E:\\Files\\KPI\\macro\\Деталь1.SLDPRT')
+swApp = start_sw(sw_version='2014', visible=1)
+swDoc = open_doc(swApp, 'E:\\Files\\KPI\\macro\\Деталь2.SLDPRT')
+
+myFeat = swDoc.FirstFeature
+while myFeat:
+    print(myFeat.GetTypeName2)
+    myFeat = myFeat.GetNextFeature
